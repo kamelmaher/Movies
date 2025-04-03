2d6b728be47f64cf54dd95957cb4fff2ea228e7a
kamelmaher <kamelmahersh@gmail.com>
2025-04-03 12:19:40 +0200

    Fix Bugs

--- src/hooks/getCategories.ts
+++ src/hooks/getCategories.ts
@@ -3,10 +3,11 @@
 import { Category } from "../types/Category";
 
 export const getCategories = (ids: number[], allCategories: Category[]) => {
-  const myCategories: Category[] = [];
-  ids.map((id) => {
-    const category = allCategories.find((e) => e.id == id)!;
-    myCategories.push(category);
+  const categories: Category[] = [];
+  allCategories.map((category) => {
+    ids.map((id) => {
+      if (category.id == id) categories.push(category);
+    });
   });
-  return myCategories;
+  return categories;
 };
--- src/hooks/getUrl.ts
+++ src/hooks/getUrl.ts
@@ -1,10 +1,11 @@
 /** @format */
 export const getUrl = (type: string, options?: any[]) => {
-  return `https://api.themoviedb.org/3/${type}?api_key=acecc2235b3b867602d49291bcc21926&${options?.map(
-    (option) => {
-      const [key, value] = Object.entries(option)[0];
-      return `${key}=${value}`;
-    }
-  )}
-  `;
+  const allOptions = options?.map((option) => {
+    const [key, value] = Object.entries(option)[0];
+    return `${key}=${value}`;
+  });
+  console.log(allOptions);
+  return `https://api.themoviedb.org/3/${type}?api_key=acecc2235b3b867602d49291bcc21926${
+    allOptions ? `&${allOptions.join("&")}` : ""
+  }`;
 };
